b0faaf9c88a2824d539ea29f7264022acab4c435
Praveen Moorthy <me@praveenster.com>
2013-04-05 04:29:55 -0700

    Make top-bottom style layout

---
+++ MainForm.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace dotnv
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxTitle = new System.Windows.Forms.TextBox();
+            this.splitContainer = new System.Windows.Forms.SplitContainer();
+            this.richTextBoxNote = new System.Windows.Forms.RichTextBox();
+            this.listBoxNotes = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).BeginInit();
+            this.splitContainer.Panel1.SuspendLayout();
+            this.splitContainer.Panel2.SuspendLayout();
+            this.splitContainer.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxTitle
+            // 
+            this.textBoxTitle.Location = new System.Drawing.Point(12, 12);
+            this.textBoxTitle.Name = "textBoxTitle";
+            this.textBoxTitle.Size = new System.Drawing.Size(644, 20);
+            this.textBoxTitle.TabIndex = 0;
+            // 
+            // splitContainer
+            // 
+            this.splitContainer.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.splitContainer.IsSplitterFixed = true;
+            this.splitContainer.Location = new System.Drawing.Point(0, 39);
+            this.splitContainer.Margin = new System.Windows.Forms.Padding(0);
+            this.splitContainer.Name = "splitContainer";
+            this.splitContainer.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer.Panel1
+            // 
+            this.splitContainer.Panel1.Controls.Add(this.listBoxNotes);
+            // 
+            // splitContainer.Panel2
+            // 
+            this.splitContainer.Panel2.Controls.Add(this.richTextBoxNote);
+            this.splitContainer.Size = new System.Drawing.Size(668, 498);
+            this.splitContainer.SplitterDistance = 222;
+            this.splitContainer.TabIndex = 2;
+            // 
+            // richTextBoxNote
+            // 
+            this.richTextBoxNote.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBoxNote.Location = new System.Drawing.Point(1, 1);
+            this.richTextBoxNote.Margin = new System.Windows.Forms.Padding(0);
+            this.richTextBoxNote.Name = "richTextBoxNote";
+            this.richTextBoxNote.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedBoth;
+            this.richTextBoxNote.Size = new System.Drawing.Size(667, 271);
+            this.richTextBoxNote.TabIndex = 0;
+            this.richTextBoxNote.Text = "";
+            // 
+            // listBoxNotes
+            // 
+            this.listBoxNotes.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listBoxNotes.FormattingEnabled = true;
+            this.listBoxNotes.IntegralHeight = false;
+            this.listBoxNotes.Location = new System.Drawing.Point(1, 1);
+            this.listBoxNotes.MultiColumn = true;
+            this.listBoxNotes.Name = "listBoxNotes";
+            this.listBoxNotes.Size = new System.Drawing.Size(667, 218);
+            this.listBoxNotes.TabIndex = 1;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(668, 537);
+            this.Controls.Add(this.splitContainer);
+            this.Controls.Add(this.textBoxTitle);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "MainForm";
+            this.Text = "dotnv";
+            this.splitContainer.Panel1.ResumeLayout(false);
+            this.splitContainer.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).EndInit();
+            this.splitContainer.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxTitle;
+        private System.Windows.Forms.SplitContainer splitContainer;
+        private System.Windows.Forms.ListBox listBoxNotes;
+        private System.Windows.Forms.RichTextBox richTextBoxNote;
+    }
+}
+
